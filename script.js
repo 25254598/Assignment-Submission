bde0e0620fe98ac2d705b36b46794ba229843ea4
25254598 <89412956+25254598@users.noreply.github.com>
2021-08-23 23:12:17 +0530

    Day 1 Assignment is submitted

---
+++ script.js
@@ -0,0 +1,10 @@
+a=10
+b=20
+Sum = a+b
+Diff = a-b
+Multiplication = a*b 
+Division = a/b 
+console.log("The sum of", a, "&", b, "is", Sum)
+console.log("The difference between", a, "&", b, "is", Diff)
+console.log("The multiplication of", a, "&", b, "is", Multiplication)
+console.log("The division of", a, "&", b, "is", Division)
